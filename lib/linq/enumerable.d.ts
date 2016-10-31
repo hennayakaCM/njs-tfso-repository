58954eb7c98f8eb3051cc9d7b6361a0f201581e3
24SevenOffice <post@24sevenoffice.com>
2016-10-31 07:37:32 +0000

    Adding release files

---
+++ lib/linq/enumerable.d.ts
@@ -0,0 +1,29 @@
+export interface IEnumerable<TEntity> extends Iterable<TEntity> {
+    where(predicate: (it: TEntity, ...param: any[]) => boolean, ...parameters: any[]): this;
+    skip(count: number): this;
+    take(count: number): this;
+    toArray(items: Array<TEntity>): Array<TEntity>;
+    toArray(): Array<TEntity>;
+    readonly operations: Operation<TEntity>;
+}
+import { Operator } from './operators/operator';
+export declare class Operation<TEntity> {
+    private _stack;
+    constructor();
+    add(operator: Operator<TEntity>): void;
+    remove(operator: Operator<TEntity>): boolean;
+    values(): IterableIterator<Operator<TEntity>>;
+}
+export default class Enumerable<TEntity> implements IEnumerable<TEntity> {
+    private items;
+    private _operations;
+    constructor(items?: Array<TEntity>);
+    where(predicate: (it: TEntity, ...param: any[]) => boolean, ...parameters: any[]): this;
+    take(count: number): this;
+    skip(count: number): this;
+    orderBy(property: (it: TEntity) => void): this;
+    toArray(items?: Array<TEntity>): Array<TEntity>;
+    readonly operations: Operation<TEntity>;
+    static fromArray<TEntity>(items?: Array<TEntity>): Enumerable<TEntity>;
+    [Symbol.iterator]: () => Iterator<TEntity>;
+}
---
+++ lib/main.d.ts
@@ -0,0 +1,3 @@
+import Repository from './repository/baserepository';
+export { default as Enumerable, IEnumerable } from './linq/enumerable';
+export default Repository;
